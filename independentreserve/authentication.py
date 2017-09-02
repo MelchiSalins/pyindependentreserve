2aaad461a3eee96576b0642cca5ccb9b0a46e3fc
Melchi Salins <melchi.salins@auspost.com.au>
2017-09-03 08:52:08 +1000

    Added get_open_orders private method

---
+++ independentreserve/authentication.py
@@ -0,0 +1,36 @@
+import requests
+import logging
+import time
+import hmac, hashlib
+import json
+from collections import OrderedDict
+
+
+class Authentication(object):
+    """
+    All private API methods require authentication. All method parameters (except signature) are required to authenticate a request. There are three additional parameters which should be passed to private API methods:
+
+    API Key
+    Nonce
+    Signature
+    """
+
+    def __init__(self, api_key, api_secret):
+        self.key = api_key
+        self.secret = api_secret
+        # self.nonce = int(time.time())
+        self.headers = {'Content-Type': 'application/json'}
+
+    def _generate_signature(self, parameters):
+        """
+        Generates a signature required to securely POST the data to the Private endpoint
+
+        :param parameters: Query params that get passed to the URL
+        :return:
+        """
+        message = ','.join(parameters)
+
+        signature = hmac.new(self.secret.encode('utf-8'),
+                             msg=message.encode('utf-8'),
+                             digestmod=hashlib.sha256).hexdigest().upper()
+        return signature
