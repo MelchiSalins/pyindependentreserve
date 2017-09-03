372a02716655c9a6a990b0b04327d4f1b23c3744
Melchi Salins <melchi.salins@auspost.com.au>
2017-09-03 18:02:17 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# Melchi
+from setuptools import setup, find_packages
+
+setup(name="pyindependentreserve",
+      version="0.1.0",
+      description="Python client for Interacting with Independent Reserve API - The Bitcoin and Digital Currency Market",
+      url="https://github.com/MelchiSalins/pyindependentreserve",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: BitCoin :: BlockChain :: Crypto Currency',
+      ],
+      keywords='Bitcoin BlockChain Crypto-currency',
+      author="Melchi Salins",
+      author_email="melchisalins@gmail.com",
+      license="MIT",
+      packages=find_packages(),
+      install_requires=['requests==2.14.2'],
+      include_package_data=True,
+      zip_safe=True,
+      )
