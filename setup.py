3bad2cd99467473739c39ac6c6fda179b17427da
Melchi Salins <Melchi.Salins@auspost.com.au>
2018-01-20 13:42:54 +1100

    Updated the release version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="pyindependentreserve",
-      version="0.1.0",
+      version="0.2.0",
       description="Python client for Interacting with Independent Reserve API - The Bitcoin and Digital Currency Market",
       url="https://github.com/MelchiSalins/pyindependentreserve",
       classifiers=[
