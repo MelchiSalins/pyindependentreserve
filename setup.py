603a49035cc605884e494062700e1f711fe5e903
Melchi <melchi@192-168-1-5.tpgi.com.au>
2021-04-27 23:41:51 +1000

    WIP: Added websockets and removed support for python2

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 from setuptools import setup, find_packages
 
 setup(name="pyindependentreserve",
-      version="0.2.0",
+      version="0.3.0",
       description="Python client for Interacting with Independent Reserve API - The Bitcoin and Digital Currency Market",
       url="https://github.com/MelchiSalins/pyindependentreserve",
       classifiers=[
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.6',
       ],
       keywords='Bitcoin BlockChain Crypto-currency',
@@ -16,7 +15,7 @@ setup(name="pyindependentreserve",
       author_email="melchisalins@gmail.com",
       license="MIT",
       packages=find_packages(),
-      install_requires=['requests==2.20.0'],
+      install_requires=['requests==2.20.0', 'websocket==8.1.0'],
       include_package_data=True,
       zip_safe=True,
       )
