75624fda4a3fd6b96d1b1790669cb1fa7285565d
Melchi <melchi@192-168-1-5.tpgi.com.au>
2021-04-28 11:41:22 +1000

    Removed cruft code

--- independentreserve/websocket.py
+++ independentreserve/websocket.py
@@ -2,18 +2,7 @@ import websockets
 import asyncio
 import sys
 
-from pprint import pprint as pp
 
-
-async def _pull_data(queue: asyncio.Queue):
-    async with websockets.connect(WSS_URL) as websocket:
-        while True:
-            data = await websocket.recv()
-            data = data.encode("utf-8")
-            await queue.put(data)
-
-
-# def subscribe(conf: IRWSSConfig = None) -> None:
 async def wss_subscribe(queue: asyncio.Queue, channel_name: list = ["ticker-xbt-aud"]):
     try:
         if len(channel_name) > 1:
