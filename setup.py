2a33746924a784ed767622af0924f3402774d0c6
MelchiS <>
2021-04-28 12:52:43 +1000

    Updated setup.py to build dist and publish to pypi

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name="pyindependentreserve",
     version="0.3.0",
     description="Python client for Interacting with Independent Reserve API - The Bitcoin and Digital Currency Market",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/MelchiSalins/pyindependentreserve",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -16,7 +18,7 @@ setup(
     author_email="melchisalins@gmail.com",
     license="MIT",
     packages=find_packages(),
-    install_requires=["requests==2.20.0", "websocket==8.1.0"],
+    install_requires=["requests==2.20.0", "websockets==8.1.0"],
     include_package_data=True,
     zip_safe=True,
 )
